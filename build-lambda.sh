8b7688511c46fe3f8b8ef8ca70703a2a86aacd4a
jvansan <jeffreyavansanten@gmail.com>
2024-02-09 14:48:44 -0800

    fix: exclude python bytecode

--- build-lambda.sh
+++ build-lambda.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+py_exclude=('*.pyc' '*__pycache__*')
+
 cd .venv/lib/python3.12/site-packages
 echo "Zipping the dependencies"
-zip -r9 ${OLDPWD}/function.zip .
+zip -r9 ${OLDPWD}/function.zip . -x "${py_exclude[@]}"
 
 echo "Zipping the app - cd to app directory ${OLDPWD}"
 cd $OLDPWD
-zip -g function.zip -r app
+zip -g function.zip -r app -x "${py_exclude[@]}"
 chmod 755 function.zip
 mkdir -p out && mv function.zip out/
